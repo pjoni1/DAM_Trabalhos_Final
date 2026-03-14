5ba047a0fbbb6132b6d67411e8aaa7660bdb51bc
pjoni <jmar1755@gmail.com>
2026-03-14 16:06:41 +0000

    calculator update

--- DAM_TP1/src/main/kotlin/dam/exer_2/exer_2.kt
+++ DAM_TP1/src/main/kotlin/dam/exer_2/exer_2.kt
@@ -18,148 +18,133 @@ fun main(){
     println("8 - Shift Left")
     println("9 - Shift Right")
 
-    val resposta = scanner.next()
-    when (resposta){
-        "1" ->{
-            println("Introduza o primeiro valor:")
-            val valor1 = scanner.nextFloat()
-            println("Introduza o segundo valor:")
-            val valor2 = scanner.nextFloat()
+    try{
+        val resposta = scanner.next()
+        when (resposta){
+            "1" ->{
+                println("Introduza o primeiro valor:")
+                val valor1 = scanner.nextFloat()
+                println("Introduza o segundo valor:")
+                val valor2 = scanner.nextFloat()
 
-            val resultado = valor1 + valor2
-            println("Resultado da soma: ")
-            println(resultado)
+                val resultado = valor1 + valor2
+                println("Resultado da soma: $resultado")
 
-            println("Resultado hexadecimal: ")
-            val bits = resultado.toBits()
-            val hex = Integer.toHexString(bits)
-            println(hex)
-        }
-        "2" ->{
-            println("Introduza o primeiro valor:")
-            val valor1 = scanner.nextFloat()
-            println("Introduza o segundo valor:")
-            val valor2 = scanner.nextFloat()
-
-            val resultado = valor1 - valor2
-            println("Resultado da subtração: ")
-            println(resultado)
-
-            println("Resultado hexadecimal: ")
-            val bits = resultado.toBits()
-            val hex = Integer.toHexString(bits)
-            println(hex)
-        }
-        "3" ->{
-            println("Introduza o primeiro valor:")
-            val valor1 = scanner.nextFloat()
-            println("Introduza o segundo valor:")
-            val valor2 = scanner.nextFloat()
-
-            val resultado = valor1 * valor2
-            println("Resultado da multiplicação: ")
-            println(resultado)
-
-            println("Resultado hexadecimal: ")
-            val bits = resultado.toBits()
-            val hex = Integer.toHexString(bits)
-            println(hex)
-        }
-        "4" ->{
-            println("Introduza o primeiro valor:")
-            val valor1 = scanner.nextFloat()
-            println("Introduza o segundo valor:")
-            val valor2 = scanner.nextFloat()
-
-            if(valor2 == 0f) {
-                println("O segundo valor não pode ser 0")
-            }else{
-                val resultado = valor1 / valor2
-                println("Resultado da divisão: ")
-                println(resultado)
-
-                println("Resultado hexadecimal: ")
                 val bits = resultado.toBits()
                 val hex = Integer.toHexString(bits)
-                println(hex)
+                println("Resultado hexadecimal: $hex")
+
             }
+            "2" ->{
+                println("Introduza o primeiro valor:")
+                val valor1 = scanner.nextFloat()
+                println("Introduza o segundo valor:")
+                val valor2 = scanner.nextFloat()
 
-        }
-        "5" ->{
-            println("Introduza o primeiro valor booleano:")
+                val resultado = valor1 - valor2
+                println("Resultado da subtração: $resultado")
 
-            val valor1 = scanner.nextBoolean()
-            println("Introduza o segundo valor booleano:")
-            val valor2 = scanner.nextBoolean()
+                val bits = resultado.toBits()
+                val hex = Integer.toHexString(bits)
+                println("Resultado hexadecimal: $hex")
 
-            if((valor1 or valor2) != (true or false)) {
-                println("Introduza valores booleanos apenas!")
-            }else{
+            }
+            "3" ->{
+                println("Introduza o primeiro valor:")
+                val valor1 = scanner.nextFloat()
+                println("Introduza o segundo valor:")
+                val valor2 = scanner.nextFloat()
+
+                val resultado = valor1 * valor2
+                println("Resultado da multiplicação: $resultado")
+
+                val bits = resultado.toBits()
+                val hex = Integer.toHexString(bits)
+                println("Resultado hexadecimal: $hex")
+            }
+            "4" ->{
+                println("Introduza o primeiro valor:")
+                val valor1 = scanner.nextFloat()
+                println("Introduza o segundo valor:")
+                val valor2 = scanner.nextFloat()
+
+                if(valor2 == 0f) {
+                    println("O segundo valor não pode ser 0")
+                }else{
+                    val resultado = valor1 / valor2
+                    println("Resultado da divisão: $resultado")
+
+                    val bits = resultado.toBits()
+                    val hex = Integer.toHexString(bits)
+                    println("Resultado hexadecimal: $hex")
+                }
+
+            }
+            "5" ->{
+                println("Introduza o primeiro valor booleano:")
+
+                val valor1 = scanner.nextBoolean()
+                println("Introduza o segundo valor booleano:")
+                val valor2 = scanner.nextBoolean()
+
                 val resultado = valor1 && valor2
-                println("Resultado da operação booleana - AND ")
-                println(resultado)
+                println("Resultado da operação booleana - AND: $resultado ")
+
             }
+            "6" ->{
+                println("Introduza o primeiro valor booleano:")
 
-        }
-        "6" ->{
-            println("Introduza o primeiro valor booleano:")
+                val valor1 = scanner.nextBoolean()
+                println("Introduza o segundo valor booleano:")
+                val valor2 = scanner.nextBoolean()
 
-            val valor1 = scanner.nextBoolean()
-            println("Introduza o segundo valor booleano:")
-            val valor2 = scanner.nextBoolean()
-
-            if((valor1 or valor2) != (true or false)) {
-                println("Introduza valores booleanos apenas!")
-            }else{
                 val resultado = valor1 || valor2
-                println("Resultado da operação booleana - OR ")
-                println(resultado)
+                println("Resultado da operação booleana - OR: $resultado ")
+
             }
+            "7" ->{
+                println("Introduza um valor booleano:")
 
-        }
-        "7" ->{
-            println("Introduza um valor booleano:")
-
-            val valor1 = scanner.nextBoolean()
+                val valor1 = scanner.nextBoolean()
 
 
-            if((valor1) != (true or false)) {
-                println("Introduza um valor booleano apenas!")
-            }else{
                 val resultado = !valor1
-                println("Resultado da operação booleana - NOT ")
-                println(resultado)
+                println("Resultado da operação booleana - NOT: $resultado")
+
             }
 
+            "8" ->{
+                println("Introduza um valor inteiro:")
+                val valor1 = scanner.nextInt()
+
+                println("Introduza o numero de posições para shiftar")
+                val valor2 = scanner.nextInt()
+
+                val resultado = valor1 shl valor2
+                println("Resultado do shift esquerdo: $resultado")
+
+
+            }
+
+            "9" ->{
+                println("Introduza um valor inteiro:")
+                val valor1 = scanner.nextInt()
+
+                println("Introduza o numero de posições para shiftar")
+                val valor2 = scanner.nextInt()
+
+                val resultado = valor1 shr valor2
+                println("Resultado do shift direito: $resultado")
+
+
+            }
+            else -> println("Opção invalida")
         }
-
-        "8" ->{
-            println("Introduza um valor inteiro:")
-            val valor1 = scanner.nextInt()
-
-            println("Introduza o numero de posições para shiftar")
-            val valor2 = scanner.nextInt()
-
-            val resultado = valor1 shl valor2
-            println("Resultado do shift esquerdo")
-            println(resultado)
-
-
-        }
-
-        "9" ->{
-            println("Introduza um valor inteiro:")
-            val valor1 = scanner.nextInt()
-
-            println("Introduza o numero de posições para shiftar")
-            val valor2 = scanner.nextInt()
-
-            val resultado = valor1 shr valor2
-            println("Resultado do shift direito")
-            println(resultado)
-
-
-        }
-        else -> println("Opção invalida")
+    }catch(e : InputMismatchException){
+        println("Erro: O valor introduzido não é válido para esta operação!")
+    }catch (e: Exception) {
+        println("Ocorreu um erro inesperado.")
     }
+
+
 }
