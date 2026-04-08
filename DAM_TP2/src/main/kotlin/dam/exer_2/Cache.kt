f6a27659be60ce13984c0f8956e51cf2a942d95d
pjoni <jmar1755@gmail.com>
2026-04-09 00:37:38 +0100

    Ex2 Cache class updated

--- DAM_TP2/src/main/kotlin/dam/exer_2/Cache.kt
+++ DAM_TP2/src/main/kotlin/dam/exer_2/Cache.kt
@@ -19,4 +19,29 @@ class Cache <K : Any, V : Any>{
         return map.size //tmb podia ser usado map.count()
     }
 
+
+    fun getOrPut(key: K, default: () -> V): V {
+        return map.getOrPut(key,default)
+
+    }
+    fun transform(key: K , action: (V) -> V): Boolean {
+        val value = map[key] //testa se existe
+
+        if(value != null){ //se existir aplica a função lambda
+            val newValue = action(value)
+            map[key] = newValue //atualiza com o novo valor
+            return true
+        }else{
+            return false
+        }
+    }
+
+    fun snapshot(): Map<K, V>{
+        val copy = map.toMap() // faz uma copia imutavel(não alteravel, não se pode aplicar metodos put ou remove)
+        return copy
+    }
+
+    //fun filterValues(predicate: (V) -> Boolean): Map<K, V>{
+
+    //}
 }
