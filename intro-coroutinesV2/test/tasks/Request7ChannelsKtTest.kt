2182885ef7a5e825f0e755b9e6f23dc57ef9fb82
pjoni <jmar1755@gmail.com>
2026-05-24 16:25:28 +0100

    first commit ex 2.3, task 1 and task 2

---
+++ intro-coroutinesV2/test/tasks/Request7ChannelsKtTest.kt
@@ -0,0 +1,29 @@
+package tasks
+
+import contributors.MockGithubService
+import contributors.concurrentProgressResults
+import contributors.testRequestData
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.currentTime
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert
+import org.junit.Test
+
+class Request7ChannelsKtTest {
+    @Test
+    fun testChannels() = runTest {
+        val startTime = currentTime
+        var index = 0
+        loadContributorsChannels(MockGithubService, testRequestData) { users, _ ->
+            val expected = concurrentProgressResults[index++]
+            val time = currentTime - startTime
+            Assert.assertEquals(
+                "Expected intermediate results after ${expected.timeFromStart} ms:",
+                expected.timeFromStart, time
+            )
+            Assert.assertEquals("Wrong intermediate results after $time:", expected.users, users)
+        }
+    }
+
+
+}
